9b0bd8b9b4a06c3b67e04bf760c19c66434ac23c
Vincent Hagen <blackshadev@users.noreply.github.com>
2023-09-16 15:02:54 +0200

    Add patch endpoint for littledivelog (#2)

---
+++ src/services/api/dives/support/convertDive.ts
@@ -0,0 +1,48 @@
+import { Salinity } from "libdivecomputerjs";
+import { Dive } from "redux/dive";
+
+interface IDiveInput {
+  date: string;
+  divetime: number;
+  fingerprint: string;
+  max_depth: number;
+  min_temperature?: number;
+  max_temperature?: number;
+  surface_temperature?: number;
+  atmospheric_pressure?: number;
+  mode?: string;
+  salinity?: Salinity;
+  tanks: {
+    pressure: { begin: number; end: number; type: 'psi' | 'bar' };
+    volume?: number;
+    oxygen?: number;
+  }[];
+  computer_id: number;
+  samples: object[];
+}
+
+export default function convertDive(computer_id: number, dive: Dive): IDiveInput {
+  return {
+    computer_id,
+    date: dive.date,
+    divetime: dive.divetime,
+    max_depth: dive.maxDepth ?? 0,
+    fingerprint: dive.fingerprint,
+    atmospheric_pressure: dive.atmospheric,
+    max_temperature: dive.maxTemperature,
+    min_temperature: dive.minTemperature,
+    mode: dive.divemode,
+    salinity: dive.salinity,
+    surface_temperature: dive.surfaceTemperature,
+    samples: dive.samples,
+    tanks: dive.tanks.map((tank) => ({
+      pressure: {
+        begin: tank.beginPressure ?? 0,
+        end: tank.endPressure ?? 0,
+        type: tank.type ?? 'bar',
+      },
+      volume: tank.volume,
+      oxygen: tank.gasmix ? Math.round(tank.gasmix.oxygen * 100) : undefined,
+    })),
+  };
+}
